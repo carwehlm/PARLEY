2270d31d7ac411693ffe9ef5d2767671e79e2e68
abertoglia <abertoglia@gmail.com>
2024-05-04 15:50:01 +0200

    Multiprocessing -Changed multiprocessing logic. Relegated process creation to java. It now actually creates multiple processes and greatly speeds up runtime.

--- run_evochecker.py
+++ run_evochecker.py
@@ -1,10 +1,8 @@
 import os
 from multiprocessing import Pool, cpu_count
-
+import subprocess
 
 def run_task(args):
-    os.chdir('Applications/EvoChecker-master')
-    os.environ['LD_LIBRARY_PATH'] = "libs/runtime"
     i, rep = args
     path = f"./{i}_{rep}.properties"
     open(path, "w").close()
@@ -15,20 +13,21 @@ def run_task(args):
         f.write("ALGORITHM = NSGAII\n")
         f.write("POPULATION_SIZE = 100\n")
         f.write("MAX_EVALUATIONS = 100\n")
-        f.write("PROCESSORS = 1\n")
+        f.write("PROCESSORS = 8\n")
         f.write("PLOT_PARETO_FRONT = false\n")
-        f.write("VERBOSE = true\n")
+        f.write("VERBOSE = false\n")
         f.write("LOAD_SEED = false\n")
         f.write(f"SEED_FILE = data/ROBOT{i}_BASELINE/front\n")
         f.write(f"INIT_PORT = 5{i}{rep}\n")
-    # Note: INIT_PORT doesn't have an effect https://github.com/gerasimou/EvoChecker/issues/11
+    # Note: The Overhead means that a higher processor count will take longer to start but will finish faster.For a pop size 100, max_eval 100, a  processor count 8 is recommended.
 
     os.system('java -jar ./target/EvoChecker-1.1.0.jar ' + path)
 
 
 def run(map_, replications):
-    # Number of parallel processes
-    num_processes = 10
+    old_cwd = os.getcwd()
+    os.chdir(f'{old_cwd}/Applications/EvoChecker-master')       #We need to change to CWD for the .jar to find itself at home
+    os.environ['LD_LIBRARY_PATH'] = "libs/runtime"
 
     # available maps
     rep_values = range(replications)  # 10 replications
@@ -36,5 +35,7 @@ def run(map_, replications):
     # Create a list of tuples with all combinations of i and rep
     tasks = [(map_, rep) for rep in rep_values]
 
-    with Pool(num_processes) as pool:
-        pool.map(run_task, tasks)
+    for task in tasks:
+        run_task(task)  #The Parallelization is being taking care by the java itself, not the python. 
+
+    os.chdir(old_cwd)       #We revert back to the original CWD for the .py to find itself at home (Yes I know there are better ways to do this.)
