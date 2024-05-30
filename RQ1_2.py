423c84d73b9fa1ea2699f67c0d5a8be8da75527f
abertoglia <abertoglia@gmail.com>
2024-05-30 08:22:28 +0200

    models up to 85

--- RQ1_2.py
+++ RQ1_2.py
@@ -9,8 +9,8 @@ import evaluation
 import plot_fronts, baseline_generator
 
 max_replications = 10       #This changes the Number of REPs.   -Should be 10
-min_maps = 81               #This changes the Number of ROBOTs. -Should be 10
-max_maps = 100               #This changes the Number of ROBOTs. -Should be 100
+min_maps = 86               #This changes the Number of ROBOTs. -Should be 10
+max_maps = 91               #This changes the Number of ROBOTs. -Should be 100
 data_folder_path = 'Applications/EvoChecker-master/data'
 evo_folder_path = "Applications/EvoChecker-master"
 
